c863c4099f61013198e59c1bc0324e4ef512636a
Andrej <andrej@sloco.sk>
2023-02-16 22:24:06 -0500

    6

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,67 @@
-
-
 <template id="app">
-  <header>
-			<h1>Rock paper scissors</h1>
-			<!-- Score board -->
-  </header>
-<div class="container">
+	<header>
+		<h1>Rock Paper Scissors</h1>
+	</header>
+	<div class="container">
 		<div class="score_board">
-		<h2>Scores:</h2>
-	<h3 class="score">You: {{player_score}}</h3>
-	<h3 class="score">Computer:
-	  {{ computer_score }}</h3>
-				<h3 class="score">Ties: {{tie}}</h3>
-		<h1 class="winner" v-if="winner">
-			{{ winner }}
-		</h1>
-	</div>
-
+			<!-- Score -->
+			<h2>Scores:</h2>
+			<h3 class="score">You: {{player_score}}</h3>
+			<h3 class="score">Computer:
+				{{ computer_score }}</h3>
+			<h3 class="score">Ties: {{tie}}</h3>
+			<h2 id="currentState"> {{ currentState }}</h2>
+		</div>
+		<!-- Player's side -->
 		<div class="float-child">
 			<div class="player">
 			</div>
 		</div>
-	<div class="main">
-		<div class="player-side">
+		<div class="main">
+			<div class="player-side">
 				<Player v-on:getChoice="updatePlayerChoice($event)" v-bind:option="player_choice"/>
 				<h2>You chose: {{player_choice}}</h2>
 				<div class="to-the-right">
-		<img  v-if="player_choice === 'rock'" v-bind:src="'./assets/' + rockImg"/>
-		<img  v-if="player_choice === 'paper'" v-bind:src="'./assets/' + paperImg" />
-		<img  v-if="player_choice === 'scissors'" v-bind:src="'./assets/' + scissorsImg" />
-		</div>
-		</div>
-		<div class="computer-side">
-		<img src="./assets/rock.png" alt="Rock" type="button" :style="[ computer_choice === 'rock' ? {opacity: `1` } : null ]">
-		<img src="./assets/paper.png" alt="Paper" type="button" :style="[ computer_choice === 'paper' ? {opacity: `1` } : null ]">
-		<img src="./assets/scissors.png" alt="Scissors" type="button" :style="[ computer_choice === 'scissors' ? {opacity: `1` } : null ]">
-		<br>
-		<h2>Computer
-		chose:
-		{{ computer_choice }}</h2>
-		<img v-if="computer_choice === 'rock'" v-bind:src="'./assets/' + rockImg" />
-		<img v-if="computer_choice === 'paper'" v-bind:src="'./assets/' + paperImg" />
-		<img v-if="computer_choice === 'scissors'" v-bind:src="'./assets/' + scissorsImg" />
+					<!-- Only the chosen one is shown on the screen -->
+					<img  v-if="player_choice === 'rock'" v-bind:src="'./assets/' + rockImg" alt="rock"/>
+					<img  v-if="player_choice === 'paper'" v-bind:src="'./assets/' + paperImg" alt="paper" />
+					<img  v-if="player_choice === 'scissors'" v-bind:src="'./assets/' + scissorsImg" alt="scissors"/>
+				</div>
+			</div>
+			<!-- Computer's side -->
+			<div class="computer-side">
+				<!-- These images are shown always -->
+				<img src="./assets/scissors.png" alt="Scissors" type="button" />
+				<img src="./assets/paper.png" alt="Paper" type="button" />
+				<img src="./assets/rock.png" alt="Rock" type="button" />
+				<br>
+				<h2>Computer
+					chose:
+					{{ computer_choice }}</h2>
+				<!-- Only the chosen one is shown on the screen -->
+				<img v-if="computer_choice === 'rock'" v-bind:src="'./assets/' + rockImg" />
+				<img v-if="computer_choice === 'paper'" v-bind:src="'./assets/' + paperImg" />
+				<img v-if="computer_choice === 'scissors'" v-bind:src="'./assets/' + scissorsImg" />
 
+			</div>
 		</div>
-</div>
-	<Computer @click="play" v-on:getChoice="updateComputerChoice($event)" v-bind:option="computer_choice"/>
+		<div class="winner-section">
+			<!-- Winner announcment -->
+			<h1 class="winner" v-if="winner">
+				{{
+								winner
+				}} </h1>
+		</div>
+		<!-- Play button -->
+		<Computer @click="play" v-on:getChoice="updateComputerChoice($event)" v-bind:option="computer_choice"/>
 
 
-</div>
+	</div>
 
 </template>
 
 <script>
+//Import components
 import Computer
 	from "@/components/Computer.vue";
 import Player
@@ -72,13 +81,15 @@ export default {
 			computer_score: "",
 			player_score: "",
 			winner: "",
-				tie: "",
-				rockImg: 'rock-d2011a36.png',
-				paperImg: 'paper-742616bc.png',
-				scissorsImg: 'scissors-33622a72.png',
+			tie: "",
+		rockImg: 'rock-d2011a36.png',
+		paperImg: 'paper-742616bc.png',
+		scissorsImg: 'scissors-33622a72.png',
+			currentState: ''
 		};
 	},
 	methods: {
+		//Updating choices
 		updatePlayerChoice(option) {
 			this.player_choice = option;
 		},
@@ -86,26 +97,35 @@ export default {
 			this.computer_choice = option;
 		},
 
-	  play() {
+		//Comparison mechanism - Game logic
+		play() {
 			const {player_choice, computer_choice} = this;
 
 			if (player_choice === computer_choice) {
 				this.winner = "It's a tie!";
 				this.tie++
 			} else if (
-		  (computer_choice === "scissors" && player_choice === "paper") ||
-		  (computer_choice === "paper" && player_choice === "rock") ||
-		  (computer_choice === "rock" && player_choice === "scissors")
-	  ) {
-			this.computer_score++
-			this.winner = 'You lost!'
-		} else if (player_choice === "") {
-		  alert("You have to select your choice!")
-	  }
-	  else {
-		  this.player_score++;
-		  this.winner = "You won!";
-	  }
+				(computer_choice === "scissors" && player_choice === "paper") ||
+				(computer_choice === "paper" && player_choice === "rock") ||
+				(computer_choice === "rock" && player_choice === "scissors")
+			) {
+				this.computer_score++
+				this.winner = 'You lost!'
+			} else if (player_choice === "") {
+				alert("Select your item!")
+			}
+			else {
+				this.player_score++;
+				this.winner = "You won!";
+			}
+			//Telling your current state
+			if (this.player_score + 1 > this.computer_score + 1) {
+				this.currentState = 'You are winning!'
+			} else if (this.player_score + 1 < this.computer_score + 1) {
+				this.currentState = 'You are loosing!'
+			} else {
+				this.currentState = 'Now the score is equal!'
+			}
 
 		}
 	}
